6795947bce604faf1a01f8d55a2b1dbf783c4982
thechocobowhisperer <the.chocobo.whisperer@gmail.com>
2019-05-07 00:08:49 -0500

    Map conversations array to display the name of each Person in Conversations

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import Chatter from './components/ChatInput/ChatInput';
 import Conversations from './components/ChatSelect/ChatSelect'
 
 class App extends Component {
-  state = {}
-
+  state = {
+    conversations : [
+                    {name: 'Zac'}, 
+                    {name: 'Person 1'}, 
+                    {name: 'Person 2'}, 
+                    {name: 'Person 3'}
+    ]
+  }
+  whatHappened = (event) => {
+    console.log(this.state.conversations)
+  }
+  
+  
   render () {
+    const convPerson = this.state.conversations.map((person, index) => {
+      return (<div><span key={index} id={index}>{person.name}</span></div>
+  )})
+      
     return (
+      
       <div className="container">
         <div className='row'>
           <div className='col-4'>
-            <Conversations/>
+            <Conversations>
+              <button onClick={(event) => this.whatHappened(event)}>Show me the console!</button>
+              {convPerson}
+            </Conversations>
           </div>
           <div className='col-8'>
             <Chatter />
--- src/components/ChatSelect/ChatSelect.js
+++ src/components/ChatSelect/ChatSelect.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import { InputGroup, Input, InputGroupAddon, Button } from 'reactstrap';
+import React, {Component} from 'react';
+
 
 const Conversations = (props) => {
-
+    
     return (
       <div className='container'>
-       <InputGroup>
-            <Input className='' onChange={(event) => this.changeName(event)} name='Name of Input' 
-            placeholder='Card Title'
-            />
-            <InputGroupAddon addonType='append'>
-                <Button
-                className=''
-                onClick={() => this.createCard()}
-                >
-                    Submit
-                </Button>
-            </InputGroupAddon>
-            
-        </InputGroup>
+       {props.children}
       </div>
     );
   };
